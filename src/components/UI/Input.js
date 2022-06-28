fcea92f86a9ba153ba8f2e15fc012ecaf55dc58e
thanush <thanushsiva06@gmail.com>
2022-06-28 18:07:37 +0530

    Shopping cart & Modal

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -5,7 +5,7 @@ import classes from './Input.module.css'
 const Input = props => {
     return (
         <div className={classes.input}>
-            <label HtmlFor={props.input.id}>{props.label}</label>
+            <label htmlFor={props.input.id}>{props.label}</label>
             <input {...props.input}/>
         </div>
     );
